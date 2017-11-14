de34c32ce905561a39f818b11ad6f23cce42c5ae
刘成胜 <liuchengsheng@jddfun.com>
2017-11-14 22:02:55 +0800

    项目初始化，添加充值rpc

---
+++ wf_data_rpc/src/main/java/com/wf/data/rpc/DataTransConvertRpcService.java
@@ -0,0 +1,13 @@
+package com.wf.data.rpc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: chengsheng.liu
+ * Date: 2017-11-14
+ * Time: 21:41
+ */
+public interface DataTransConvertRpcService {
+
+    Double findUserSumRecharge(Long userId);
+}
