795b3adb1c239677cdd4428f7441661bd9ea217b
刘成胜 <liuchengsheng@jddfun.com>
2017-11-16 15:28:22 +0800

    初始化

--- wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClientPoolFactory.java
+++ wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClientPoolFactory.java
@@ -1,58 +1,50 @@
 package com.wf.data.common.utils.elasticsearch;
 
-import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.elasticsearch.client.transport.TransportClient;
 
 import java.net.InetAddress;
 
 @Component
-public class EsClientPoolFactory implements PoolableObjectFactory {
+public class EsClientPoolFactory extends BasePooledObjectFactory<EsClient> {
 
-	@Autowired
-	private EsBean esBean;
-	
-	@Override
-	public void activateObject(Object obj) throws Exception {
-		//TODO
-	}
+    @Autowired
+    private EsBean esBean;
 
-	@Override
-	public void destroyObject(Object obj) throws Exception {
-		toClient(obj).client.close();
-	}
 
-	@Override
-	public Object makeObject() throws Exception {
-		Settings settings = Settings.builder().put("cluster.name", esBean.getClusterName()).put("node.name", esBean.getNodeName()).build();
-		@SuppressWarnings("resource")
+
+    @Override
+    public EsClient create() throws Exception {
+
+        Settings settings = Settings.builder().put("cluster.name", esBean.getClusterName()).put("node.name", esBean.getNodeName()).build();
+        @SuppressWarnings("resource")
         Client client = new PreBuiltTransportClient(settings);
-		for (String ip : esBean.getIps().split(",")) {
-			try {
-				((TransportClient) client).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), Integer.valueOf(esBean.getPort())));
-			} catch (Exception e) {
-				throw new RuntimeException("连接es请求失败：", e);
-			}
-		}
-		return new EsClient(client);
-	}
+        for (String ip : esBean.getIps().split(",")) {
+            try {
+                ((TransportClient) client).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), Integer.valueOf(esBean.getPort())));
+            } catch (Exception e) {
+                throw new RuntimeException("连接es请求失败：", e);
+            }
+        }
+        return new EsClient(client);
+    }
 
-	@Override
-	public void passivateObject(Object obj) throws Exception {
-		//TODO
-	}
+    @Override
+    public PooledObject<EsClient> wrap(EsClient obj) {
+        return new DefaultPooledObject<EsClient>(obj);
+    }
 
-	@Override
-	public boolean validateObject(Object obj) {
-		return toClient(obj).isActive();
-	}
-	
-	private EsClient toClient(Object obj) {
-		return (EsClient) obj;
-	}
+    @Override
+    public void passivateObject(PooledObject<EsClient> p) throws Exception {
+        super.passivateObject(p);
+    }
+
 }
