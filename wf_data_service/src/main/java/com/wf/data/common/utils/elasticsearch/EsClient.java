1ba80082fa1f59aac6ca119b0f3e88db03fe2520
刘成胜 <liuchengsheng@jddfun.com>
2017-11-16 17:58:11 +0800

    初始化

--- wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClient.java
+++ wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClient.java
@@ -1,15 +1,17 @@
 package com.wf.data.common.utils.elasticsearch;
 
-import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
 
 public class EsClient {
-	public final Client client;
-	private final long createTime;
-	public EsClient(Client client) {
-		this.client = client;
-		this.createTime = System.currentTimeMillis();
-	}
-	public boolean isActive() {
-		return System.currentTimeMillis() - createTime > 1000 * 60 * 60l;
-	}
+    public final TransportClient client;
+    private final long createTime;
+
+    public EsClient(TransportClient client) {
+        this.client = client;
+        this.createTime = System.currentTimeMillis();
+    }
+
+    public boolean isActive() {
+        return System.currentTimeMillis() - createTime > 1000 * 60 * 60l;
+    }
 }
--- wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClientPoolFactory.java
+++ wf_data_service/src/main/java/com/wf/data/common/utils/elasticsearch/EsClientPoolFactory.java
@@ -3,13 +3,12 @@ package com.wf.data.common.utils.elasticsearch;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
-import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.elasticsearch.client.transport.TransportClient;
 
 import java.net.InetAddress;
 
@@ -20,16 +19,15 @@ public class EsClientPoolFactory extends BasePooledObjectFactory<EsClient> {
     private EsBean esBean;
 
 
-
     @Override
     public EsClient create() throws Exception {
 
-        Settings settings = Settings.builder().put("cluster.name", esBean.getClusterName()).put("node.name", esBean.getNodeName()).build();
+        Settings settings = Settings.builder().put("cluster.name", esBean.getClusterName()).put("node.name", esBean.getNodeName()).put("client.transport.sniff", true).build();
         @SuppressWarnings("resource")
-        Client client = new PreBuiltTransportClient(settings);
+        TransportClient client = new PreBuiltTransportClient(settings);
         for (String ip : esBean.getIps().split(",")) {
             try {
-                ((TransportClient) client).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), Integer.valueOf(esBean.getPort())));
+                client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), Integer.valueOf(esBean.getPort())));
             } catch (Exception e) {
                 throw new RuntimeException("连接es请求失败：", e);
             }
