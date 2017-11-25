1f2a8af1f6041143268aac07ddf638101e4d4723
刘成胜 <liuchengsheng@jddfun.com>
2017-11-25 19:26:44 +0800

    埋点异步

--- wf_data_service/src/main/java/com/wf/data/service/BehaviorRecordService.java
+++ wf_data_service/src/main/java/com/wf/data/service/BehaviorRecordService.java
@@ -3,11 +3,13 @@ package com.wf.data.service;
 import com.wf.core.service.CrudService;
 import com.wf.data.dao.mycatdata.BehaviorRecordDao;
 import com.wf.data.dao.mycatdata.entity.BehaviorRecord;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BehaviorRecordService extends CrudService<BehaviorRecordDao, BehaviorRecord> {
 
+	@Async
 	@Override
 	public void save(BehaviorRecord entity) {
 		if(entity != null && entity.getUserId() == null){
