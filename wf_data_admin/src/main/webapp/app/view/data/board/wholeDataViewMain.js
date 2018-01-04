8d4922e205ac1bc733926522cc450237e2fe8109
黄健健 <huangjianjian@jddfun.com>
2018-01-04 18:29:58 +0800

    数据看板代码提交(六)

--- wf_data_admin/src/main/webapp/app/view/data/board/wholeDataViewMain.js
+++ wf_data_admin/src/main/webapp/app/view/data/board/wholeDataViewMain.js
@@ -3,61 +3,21 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
     title: '数据概览-整体数据概览',
     xtype: 'wholeDataViewMain',
     closable: true,
+    autoScroll:true,
     layout: {
         type: 'vbox',
         align: 'stretch'
     },
-    config:{
-        option: {
-            0:{
-                title: {
-                    text: 'ECharts 入门示例'
-                },
-                tooltip: {},
-                legend: {
-                    data:['销量']
-                },
-                xAxis: {
-                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-                },
-                yAxis: {
-                },
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
-            },
-            1:{
-                title: {
-                    text: 'ECharts 入门示例'
-                },
-                tooltip: {},
-                legend: {
-                    data:['销量']
-                },
-                xAxis: {
-                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-                },
-                yAxis: {
-                },
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 30]
-                }]
-            }
-        }
-    },
+
     initComponent: function () {
         var me = this;
-        // me.on("boxready", function () {
-        //     me.echarts = echarts.init(me.getEl().dom);
-        //     if (me.option) {
-        //         me.echarts.setOption(me.option[0]);
-        //     }
-        // });
-        me.callParent();
+        me.on("boxready", function () {
+            for (var i = 1; i <= 3; i++) {
+                me.echarts = echarts.init(Ext.get("kpi"+i).dom);
+                me.echarts.setOption(me.option[i]);
+            }
+        });
+        me.callParent(arguments);
         var store = Ext.create('DCIS.Store', {
             autoLoad: true,
             url: 'data/admin/tcard/getAnalysisList.do',
@@ -142,25 +102,72 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
         });
 
         me.add({
-            type: 'vbox',
-            align: 'stretch',
-            width: 500,
-            forceFit:true,
+            xtype:'panel',
+            layout:'vbox',
+            align : 'stretch',
+            bodyStyle:'border-width:0 0 0 0;',
             items: [{
-                title: '核心指标', html: 'flex:1', height:300, type:"hbox",
-                items:[{
-                    title:'DAU',html:'1',
-                    title:'充值金额',html:'2',
-                    title:'投注人数',html:'3',
-                }]
+                title: '核心指标',align:'stretch',height:900,width:"100%",xtype:"panel",layout:'hbox',forceFit:true,bodyStyle:'border-width:0 0 0 0;',
+                items:[
+                    {title:'DAU',width:"33.33%",height:300,id:"kpi1"},
+                    {title:'充值金额',width:"33.33%",height:300,id:"kpi2"},
+                    {title:'投注人数',width:"33.33%",height:300,id:"kpi3"}]
             }, {
-                title: '投注数据', html: 'height:150',height:300
-            }, {
-                title: '付费数据', html: 'flex:2',height:300
+                title: '投注数据', html: 'height:150',height:300, align : 'stretch',width:"100%", xtype:"panel",layout:'hbox',forceFit:true,bodyStyle:'border-width:0 0 0 0;',
+                items:[
+                    {title:'每日投注流水',html:'11111',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'},
+                    {title:'返奖率',html:'2222',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'},
+                    // {title:'投注人数',html:'3333',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'}
+                    ]
+            },  {
+                title: '付费数据', html: 'flex:2',height:300, align : 'stretch', width:"100%",xtype:"panel",layout:'hbox',forceFit:true,bodyStyle:'border-width:0 0 0 0;',
+                items:[
+                    {title:'DAU',html:'11111',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'},
+                    {title:'充值金额',html:'2222',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'},
+                    {title:'投注人数',html:'3333',width:"33.33%",bodyStyle:'border-width:0 0 0 0;'
+                    }]
             }]
         });
-
-
-
+    },
+    config:{
+        option:
+            {
+           1:{
+               title: {text: 'dau'},
+               tooltip: {},
+               legend: {data: ['日活']},
+               xAxis: {data: [1, 2, 3, 4, 5, 6]},
+               yAxis: {},
+               series: [{
+                   name: '日活',
+                   type: 'line',
+                   data: [5, 20, 36, 10, 10, 20]
+               }]
+             },
+            2:{
+                title: {text: '充值金额'},
+                tooltip: {},
+                legend: {data: ['充值金额']},
+                xAxis: {data: [3, 4, 5, 6, 7, 8]},
+                yAxis: {},
+                series: [{
+                    name: '充值金额',
+                    type: 'line',
+                    data: [1, 2, 3, 4, 5, 6]
+                }]
+            },
+            3:{
+                title: {text: '充值金额'},
+                tooltip: {},
+                legend: {data: ['充值金额']},
+                xAxis: {data: [3, 4, 5, 6, 7, 8]},
+                yAxis: {},
+                series: [{
+                    name: '充值金额',
+                    type: 'line',
+                    data: [1, 2, 3, 4, 5, 6]
+                }]
+            },
+            }
     }
 });
