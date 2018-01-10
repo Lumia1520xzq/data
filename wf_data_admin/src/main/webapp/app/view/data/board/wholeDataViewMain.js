d4de0cadf4bc8d3f1599f83c3cfaad4c076bf93d
黄健健 <huangjianjian@jddfun.com>
2018-01-10 16:26:42 +0800

    数据看板二期(四)

--- wf_data_admin/src/main/webapp/app/view/data/board/wholeDataViewMain.js
+++ wf_data_admin/src/main/webapp/app/view/data/board/wholeDataViewMain.js
@@ -187,6 +187,7 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
                 totalCost[i]=re.get('totalCost');
                 costRate[i]=re.get('costRate');
             }
+
             var option = [
                 {
                     title: {text: 'DAU'},
@@ -860,17 +861,17 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
                     }]
                 },
                 {
-                    title: {text: '新用户占比'},
+                    title: {text: '成本'},
                     tooltip: {trigger: 'axis',
                         formatter: function (params) {
                             var str='';
                             for(var i = 0; i < params.length; i++){
-                                str += '日期:'+params[i].name+'<br/>'+ params[i].seriesName +':' + params[i].value + '%';
+                                str += '日期:'+params[i].name+'<br/>'+ params[i].seriesName +':' + params[i].value;
                             }
                             return str;
                         }
                     },
-                    // legend: {data: ['新用户占比']},
+                    // legend: {data: ['成本']},
                     toolbox: {
                         show : true,
                         x : 450,
@@ -890,16 +891,16 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
                     yAxis: {
                         type : 'value',
                         axisLabel : {
-                            formatter: '{value}%'
+                            formatter: '{value}'
                         }
                     },
                     series: [{
-                        name: '新用户占比',
+                        name: '成本',
                         type: 'line',
                         smooth: true,
                         // itemStyle: {normal: {areaStyle: {type: 'default'}}},
                         itemStyle: {normal: {}},
-                        data: usersRate
+                        data:totalCost
                     }]
                 },
                 {
@@ -943,6 +944,49 @@ Ext.define('WF.view.data.board.wholeDataViewMain', {
                         // itemStyle: {normal: {areaStyle: {type: 'default'}}},
                         itemStyle: {normal: {}},
                         data:totalCost
+                    }]
+                },
+                {
+                    title: {text: '成本占比'},
+                    tooltip: {trigger: 'axis',
+                        formatter: function (params) {
+                            var str='';
+                            for(var i = 0; i < params.length; i++){
+                                str += '日期:'+params[i].name+'<br/>'+ params[i].seriesName +':' + params[i].value + '%';
+                            }
+                            return str;
+                        }
+                    },
+                    // legend: {data: ['成本占比']},
+                    toolbox: {
+                        show : true,
+                        x : 450,
+                        feature : {
+                            mark : {show: true},
+                            // dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis: {
+                        type : 'category',
+                        boundaryGap : false,
+                        data: businessDate
+                    },
+                    yAxis: {
+                        type : 'value',
+                        axisLabel : {
+                            formatter: '{value}%'
+                        }
+                    },
+                    series: [{
+                        name: '成本占比',
+                        type: 'line',
+                        smooth: true,
+                        // itemStyle: {normal: {areaStyle: {type: 'default'}}},
+                        itemStyle: {normal: {}},
+                        data:costRate
                     }]
                 }
             ];
