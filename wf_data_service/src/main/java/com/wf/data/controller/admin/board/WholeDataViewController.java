94912a008fdb4eda35ef44a32053931e18bf01c1
黄健健 <huangjianjian@jddfun.com>
2018-01-10 14:07:41 +0800

    数据看板二期(二)

--- wf_data_service/src/main/java/com/wf/data/controller/admin/board/WholeDataViewController.java
+++ wf_data_service/src/main/java/com/wf/data/controller/admin/board/WholeDataViewController.java
@@ -7,14 +7,18 @@ import com.wf.core.utils.type.StringUtils;
 import com.wf.core.web.base.ExtJsController;
 import com.wf.data.common.utils.DateUtils;
 import com.wf.data.dao.base.entity.ChannelInfo;
+import com.wf.data.dao.data.entity.DatawareFinalChannelInfoAll;
+import com.wf.data.dao.data.entity.DatawareFinalChannelRetention;
 import com.wf.data.service.ChannelInfoService;
 import com.wf.data.service.data.DatawareFinalChannelInfoAllService;
+import com.wf.data.service.data.DatawareFinalChannelRetentionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,10 +34,11 @@ public class WholeDataViewController extends ExtJsController {
     private DatawareFinalChannelInfoAllService datawareFinalChannelInfoAllService;
     @Autowired
     private ChannelInfoService channelInfoService;
+    @Autowired
+    private DatawareFinalChannelRetentionService datawareFinalChannelRetentionService;
 
     /**
      * 整体数据概览
-     * @return
      */
     @RequestMapping("/getList")
     public Object getList() {
@@ -75,7 +80,20 @@ public class WholeDataViewController extends ExtJsController {
         }
             params.put("beginDate",startTime);
             params.put("endDate",endTime);
-        return  datawareFinalChannelInfoAllService.getListByChannelAndDate(params);
+            List<DatawareFinalChannelInfoAll> allList = datawareFinalChannelInfoAllService.getListByChannelAndDate(params);
+            for(DatawareFinalChannelInfoAll info:allList) {
+                String date = info.getBusinessDate();
+                DatawareFinalChannelRetention retention = datawareFinalChannelRetentionService.findByDate(date);
+                if(null != retention) {
+                    Double usersDayRetention = retention.getUsersDayRetention();
+                    Double dayRetention = retention.getDayRetention();
+                    Double usersRate = retention.getUsersRate();
+                    info.setUsersDayRetention(usersDayRetention);
+                    info.setDayRetention(dayRetention);
+                    info.setUsersRate(usersRate);
+                }
+            }
+            return  datawareFinalChannelInfoAllService.getListByChannelAndDate(params);
     }
 
 
--- wf_data_service/src/main/java/com/wf/data/dao/data/entity/DatawareFinalChannelInfoAll.java
+++ wf_data_service/src/main/java/com/wf/data/dao/data/entity/DatawareFinalChannelInfoAll.java
@@ -25,6 +25,9 @@ public class DatawareFinalChannelInfoAll extends DataEntity {
 	private Long channelId;
 	private String businessDate;
 	private Long userBettingCount;
+	private Double usersDayRetention;
+	private Double dayRetention;
+	private Double usersRate;
 
 	public Long getUserBettingCount() {
 		return userBettingCount;
@@ -184,5 +187,29 @@ public class DatawareFinalChannelInfoAll extends DataEntity {
 
 	public void setBusinessDate(String businessDate) {
 		this.businessDate = businessDate;
+	}
+
+	public Double getUsersDayRetention() {
+		return usersDayRetention;
+	}
+
+	public void setUsersDayRetention(Double usersDayRetention) {
+		this.usersDayRetention = usersDayRetention;
+	}
+
+	public Double getDayRetention() {
+		return dayRetention;
+	}
+
+	public void setDayRetention(Double dayRetention) {
+		this.dayRetention = dayRetention;
+	}
+
+	public Double getUsersRate() {
+		return usersRate;
+	}
+
+	public void setUsersRate(Double usersRate) {
+		this.usersRate = usersRate;
 	}
 }
