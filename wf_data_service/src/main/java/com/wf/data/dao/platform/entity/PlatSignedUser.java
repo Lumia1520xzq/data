504b4ae273b8ea076b9be6781d9dc0941153d5a4
刘成胜 <liuchengsheng@jddfun.com>
2018-01-24 11:16:59 +0800

    老签到

--- wf_data_service/src/main/java/com/wf/data/dao/platform/entity/PlatSignedUser.java
+++ wf_data_service/src/main/java/com/wf/data/dao/platform/entity/PlatSignedUser.java
@@ -10,4 +10,44 @@ public class PlatSignedUser extends DataEntity {
 	private Long amount;
 	private Integer status;
 	private Long channelId;
+
+	public Integer getDays() {
+		return days;
+	}
+
+	public void setDays(Integer days) {
+		this.days = days;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Long getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(Long channelId) {
+		this.channelId = channelId;
+	}
 }
