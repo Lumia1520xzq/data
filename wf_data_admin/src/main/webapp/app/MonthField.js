ab37db261a4f27f537022f2e21166f5a12ad260b
黄健健 <huangjianjian@jddfun.com>
2018-01-24 13:05:39 +0800

    月度指标监控(六)

--- wf_data_admin/src/main/webapp/app/MonthField.js
+++ wf_data_admin/src/main/webapp/app/MonthField.js
@@ -1,116 +1,79 @@
-Ext.define('Ext.ux.form.MonthField', {
-    extend: 'Ext.form.field.Picker',
+Ext.define('Ext.form.field.Month', {
+    extend: 'Ext.form.field.Date',
     alias: 'widget.monthfield',
-    //requires: ['Ext.picker.Date'],
-    //alternateClassName: ['Ext.form.DateField', 'Ext.form.Date'],
-
-    format: "Y-m",
-
-    altFormats: "m/y|m/Y|m-y|m-Y|my|mY|y/m|Y/m|y-m|Y-m|ym|Ym",
-
-    //disabledDaysText: "Disabled",
-
-    //disabledDatesText: "Disabled",
-
-    //minText: "The date in this field must be equal to or after {0}",
-
-    //maxText: "The date in this field must be equal to or before {0}",
-
-    //invalidText: "{0} is not a valid date - it must be in the format {1}",
-
-    triggerCls: Ext.baseCSSPrefix + 'form-date-trigger',
-
-    //showToday: true,
-
-    //initTime: '12',
-
-    //initTimeFormat: 'H',
-
-    matchFieldWidth: false,
-
-    startDay: new Date(),
-
-    initComponent: function () {
-        var me = this;
-
-
-        me.disabledDatesRE = null;
-
-        me.callParent();
-    },
-
-    initValue: function () {
-        var me = this,
-            value = me.value;
-
-        if (Ext.isString(value)) {
-            me.value = Ext.Date.parse(value, this.format);
-        }
-        if (me.value)
-            me.startDay = me.value;
-        me.callParent();
-    },
-
-    rawToValue: function (rawValue) {
-        return Ext.Date.parse(rawValue, this.format) || rawValue || null;
-    },
-
-    valueToRaw: function (value) {
-        return this.formatDate(value);
-    },
-
-
-
-    formatDate: function (date) {
-        return Ext.isDate(date) ? Ext.Date.dateFormat(date, this.format) : date;
-    },
+    requires: ['Ext.picker.Month'],
+    alternateClassName: ['Ext.form.MonthField', 'Ext.form.Month'],
+    selectMonth: null,
     createPicker: function () {
         var me = this,
-            format = Ext.String.format;
-
-        return Ext.create('Ext.picker.Month', {
-            //renderTo: me.el,
+            format = Ext.String.format,
+            pickerConfig;
+        pickerConfig = {
             pickerField: me,
-            ownerCt: me.ownerCt,
+            ownerCmp: me,
             renderTo: document.body,
             floating: true,
-            shadow: false,
+            hidden: true,
             focusOnShow: true,
+            minDate: me.minValue,
+            maxDate: me.maxValue,
+            disabledDatesRE: me.disabledDatesRE,
+            disabledDatesText: me.disabledDatesText,
+            disabledDays: me.disabledDays,
+            disabledDaysText: me.disabledDaysText,
+            format: me.format,
+            showToday: me.showToday,
+            startDay: me.startDay,
+            minText: format(me.minText, me.formatDate(me.minValue)),
+            maxText: format(me.maxText, me.formatDate(me.maxValue)),
             listeners: {
-                scope: me,
-                cancelclick: me.onCancelClick,
-                okclick: me.onOkClick,
-                yeardblclick: me.onOkClick,
-                monthdblclick: me.onOkClick
+                select: { scope: me, fn: me.onSelect },
+                monthdblclick: { scope: me, fn: me.onOKClick },
+                yeardblclick: { scope: me, fn: me.onOKClick },
+                OkClick: { scope: me, fn: me.onOKClick },
+                CancelClick: { scope: me, fn: me.onCancelClick }
+            },
+            keyNavConfig: {
+                esc: function () {
+                    me.collapse();
+                }
             }
-        });
+        };
+        if (Ext.isChrome) {
+            me.originalCollapse = me.collapse;
+            pickerConfig.listeners.boxready = {
+                fn: function () {
+                    this.picker.el.on({
+                        mousedown: function () {
+                            this.collapse = Ext.emptyFn;
+                        },
+                        mouseup: function () {
+                            this.collapse = this.originalCollapse;
+                        },
+                        scope: this
+                    });
+                },
+                scope: me,
+                single: true
+            }
+        }
+        return Ext.create('Ext.picker.Month', pickerConfig);
     },
-
-    onExpand: function () {
-        //this.picker.show();
-        this.picker.setValue(this.startDay);
-        //
-
-    },
-
-    //    onCollapse: function () {
-    //        this.focus(false, 60);
-    //    },
-
-    onOkClick: function (picker, value) {
-        var me = this,
-            month = value[0],
-            year = value[1],
-            date = new Date(year, month, 1);
-        me.startDay = date;
-        me.setValue(date);
-        this.picker.hide();
-        //this.blur();
-    },
-
     onCancelClick: function () {
-        this.picker.hide();
-        //this.blur();
+        var me = this;
+        me.selectMonth = null;
+        me.collapse();
+    },
+    onOKClick: function () {
+        var me = this;
+        if (me.selectMonth) {
+            me.setValue(me.selectMonth);
+            me.fireEvent('select', me, me.selectMonth);
+        }
+        me.collapse();
+    },
+    onSelect: function (m, d) {
+        var me = this;
+        me.selectMonth = new Date((d[0] + 1) + '/1/' + d[1]);
     }
-
 });
