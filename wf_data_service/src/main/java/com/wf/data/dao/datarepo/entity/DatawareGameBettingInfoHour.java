e855119a47e9ba4581a41ec518ed9153b142de97
shihuibeeplay <shihui@beeplay123.com>
2018-02-07 17:42:36 +0800

    新增游戏小时数据清洗表

---
+++ wf_data_service/src/main/java/com/wf/data/dao/datarepo/entity/DatawareGameBettingInfoHour.java
@@ -0,0 +1,117 @@
+package com.wf.data.dao.datarepo.entity;
+
+import com.wf.core.persistence.DataEntity;
+
+public class DatawareGameBettingInfoHour extends DataEntity {
+    private static final long serialVersionUID = -1l;
+    private Long parentId;
+    private Long channelId;
+    private Integer gameType;
+    private Long hourDau;
+    private Long dau;
+    private Double hourBettingAmount;
+    private Long hourBettingCount;
+    private Long hourBettingUserCount;
+    private Long bettingUserCount;
+    private Double hourReturnAmount;
+    private String businessHour;
+    private String businessDate;
+
+    public Long getParentId() {
+        return this.parentId;
+    }
+
+    public void setParentId(Long value) {
+        this.parentId = value;
+    }
+
+    public Long getChannelId() {
+        return this.channelId;
+    }
+
+    public void setChannelId(Long value) {
+        this.channelId = value;
+    }
+
+    public Integer getGameType() {
+        return this.gameType;
+    }
+
+    public void setGameType(Integer value) {
+        this.gameType = value;
+    }
+
+    public Long getHourDau() {
+        return this.hourDau;
+    }
+
+    public void setHourDau(Long value) {
+        this.hourDau = value;
+    }
+
+    public Long getDau() {
+        return this.dau;
+    }
+
+    public void setDau(Long value) {
+        this.dau = value;
+    }
+
+    public Double getHourBettingAmount() {
+        return this.hourBettingAmount;
+    }
+
+    public void setHourBettingAmount(Double value) {
+        this.hourBettingAmount = value;
+    }
+
+    public Long getHourBettingCount() {
+        return this.hourBettingCount;
+    }
+
+    public void setHourBettingCount(Long value) {
+        this.hourBettingCount = value;
+    }
+
+    public Long getHourBettingUserCount() {
+        return this.hourBettingUserCount;
+    }
+
+    public void setHourBettingUserCount(Long value) {
+        this.hourBettingUserCount = value;
+    }
+
+    public Long getBettingUserCount() {
+        return this.bettingUserCount;
+    }
+
+    public void setBettingUserCount(Long value) {
+        this.bettingUserCount = value;
+    }
+
+    public Double getHourReturnAmount() {
+        return this.hourReturnAmount;
+    }
+
+    public void setHourReturnAmount(Double value) {
+        this.hourReturnAmount = value;
+    }
+
+    public String getBusinessHour() {
+        return this.businessHour;
+    }
+
+    public void setBusinessHour(String value) {
+        this.businessHour = value;
+    }
+
+    public String getBusinessDate() {
+        return this.businessDate;
+    }
+
+    public void setBusinessDate(String value) {
+        this.businessDate = value;
+    }
+
+}
+
---
+++ wf_data_service/src/main/java/com/wf/data/service/data/DatawareGameBettingInfoHourService.java
@@ -0,0 +1,14 @@
+package com.wf.data.service.data;
+
+
+import com.wf.core.service.CrudService;
+import com.wf.data.dao.datarepo.DatawareGameBettingInfoHourDao;
+import com.wf.data.dao.datarepo.entity.DatawareGameBettingInfoHour;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DatawareGameBettingInfoHourService extends CrudService<DatawareGameBettingInfoHourDao,DatawareGameBettingInfoHour>{
+
+}
+
