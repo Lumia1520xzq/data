7a3dda1e6384426068fadd396536597d08f1966a
刘成胜 <liuchengsheng@jddfun.com>
2018-03-12 11:18:28 +0800

    用户净盈利

---
+++ wf_data_admin/src/main/webapp/app/view/business/userProfitMain.js
@@ -0,0 +1,153 @@
+Ext.define('WF.view.business.userProfitMain', {
+    extend: 'Ext.panel.Panel',
+    title: '用户净盈利信息',
+    xtype: 'userProfitMain',
+    closable: true,
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    initComponent: function () {
+        this.callParent(arguments);
+        var me = this;
+        var store = Ext.create('DCIS.Store', {
+            autoLoad: true,
+            url: 'data/admin/business/profit/list.do',
+            fields: [
+                'userName', 'userId', 'profitAmount',
+                'totalRechargeAmount', 'totalWarsProfit', 'costAmount',
+                'totalWarsBetting', 'useGoldAmount'
+            ]
+        });
+
+        me.add({
+            border: false,
+            store: store,
+            xtype: 'searchpanel',
+            title: '查询',
+            collapsible: true,
+            collapsed: false,
+            columns: 2,
+            buildField: "Manual",
+            forceFit: false,
+            items: [
+                {
+                    colspan: 1,
+                    name: 'userId',
+                    xtype: 'searchfield',
+                    displayField: 'nickname',
+                    valueField: "id",
+                    queryMode: "local",
+                    store: 'userStore',
+                    editable: false,
+                    fieldLabel: '用户'
+                }]
+        });
+        me.add({
+            xtype: 'datagrid',
+            store: store,
+            buildField: "Manual",
+            forceFit: true,
+            columns: [
+                {
+                    text: '用户昵称',
+                    width: 50,
+                    dataIndex: 'userName',
+                    menuDisabled: true,
+                    sortable: false
+                },
+                {
+                    text: '游戏用户ID',
+                    dataIndex: 'userId',
+                    width: 50,
+                    menuDisabled: true,
+                    sortable: false
+                },
+                {
+                    text: 'P',//盈利
+                    width: 50,
+                    dataIndex: 'profitAmount',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                },
+                {
+                    text: 'B1',//充值
+                    width: 50,
+                    dataIndex: 'totalRechargeAmount',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                },
+                {
+                    text: 'B2',//向其它用户获取金叶子
+                    width: 50,
+                    dataIndex: 'totalWarsProfit',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                },
+                {
+                    text: 'S1',//实物出口
+                    width: 50,
+                    dataIndex: 'costAmount',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                },
+                {
+                    text: 'S2',//向其它用户输出金叶子
+                    width: 50,
+                    dataIndex: 'totalWarsBetting',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                },
+                {
+                    text: 'S3',//剩余金叶子
+                    width: 30,
+                    dataIndex: 'useGoldAmount',
+                    menuDisabled: true,
+                    sortable: false,
+                    renderer: function (value) {
+                        if (value == null) {
+                            return '0.0000';
+                        } else {
+                            return Ext.util.Format.number(value, "0,000");
+                        }
+                    }
+                }
+            ]
+        });
+    }
+});
---
+++ wf_data_service/src/main/java/com/wf/data/controller/admin/business/UserProfitController.java
@@ -0,0 +1,17 @@
+package com.wf.data.controller.admin.business;
+
+import com.wf.core.web.base.ExtJsController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: lcs
+ * @date: 2018/03/12
+ */
+@RestController
+@RequestMapping("/data/admin/business/profit")
+public class UserProfitController extends ExtJsController {
+
+
+
+}
