a050838df6a2721cb3c7356343c1a440b2f9e16a
shihuibeeplay <shihui@beeplay123.com>
2018-04-17 13:43:20 +0800

    奖多多彩票用户ID推送

--- wf_data_rpc/src/main/java/com/wf/data/rpc/dto/JsonResult.java
+++ wf_data_rpc/src/main/java/com/wf/data/rpc/dto/JsonResult.java
@@ -9,7 +9,6 @@ import java.io.Serializable;
  * @date 2014-7-5下午3:26:41
  */
 public class JsonResult<T> implements Serializable {
-	private static final long serialVersionUID = 1905122041950251207L;
 
 	/**
 	 * 请求处理是否成功
